b5777520c2f86b7ec2967060c1c87deda3272377
Patrick Maloy <prmaloy@gmail.com>
2020-09-28 14:51:52 -0700

    Updates

---
+++ precache-manifest.7875faba58c44550a1c22b287491e439.js
@@ -0,0 +1,278 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "128c4793a62a65934691afd0496d8dbf",
+    "url": "/prmaloy/Surreal.git/index.html"
+  },
+  {
+    "revision": "4f7010bc6ca1626ca0ec",
+    "url": "/prmaloy/Surreal.git/static/css/14.7550f3b0.chunk.css"
+  },
+  {
+    "revision": "2581d54279af53bb73e3",
+    "url": "/prmaloy/Surreal.git/static/css/5.9522b9fe.chunk.css"
+  },
+  {
+    "revision": "11a2d57a25257a8dad00",
+    "url": "/prmaloy/Surreal.git/static/css/main.129133ad.chunk.css"
+  },
+  {
+    "revision": "effedbb1767d874492e1",
+    "url": "/prmaloy/Surreal.git/static/js/0.73e82ab2.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/prmaloy/Surreal.git/static/js/0.73e82ab2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39c2609346614cb2ce3e",
+    "url": "/prmaloy/Surreal.git/static/js/10.5fe288d7.chunk.js"
+  },
+  {
+    "revision": "ed1875ca0094fdac8093",
+    "url": "/prmaloy/Surreal.git/static/js/11.88279e7e.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/prmaloy/Surreal.git/static/js/11.88279e7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9555c4da8de08fc3b119",
+    "url": "/prmaloy/Surreal.git/static/js/12.22e4be0d.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/prmaloy/Surreal.git/static/js/12.22e4be0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3034b5d6362016c50735",
+    "url": "/prmaloy/Surreal.git/static/js/13.38124be4.chunk.js"
+  },
+  {
+    "revision": "4f7010bc6ca1626ca0ec",
+    "url": "/prmaloy/Surreal.git/static/js/14.45f23e74.chunk.js"
+  },
+  {
+    "revision": "1b7c64cd9beb6a479056",
+    "url": "/prmaloy/Surreal.git/static/js/15.fdd0e998.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/prmaloy/Surreal.git/static/js/15.fdd0e998.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "906be0a438a9081185d3",
+    "url": "/prmaloy/Surreal.git/static/js/16.7694dce0.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/prmaloy/Surreal.git/static/js/16.7694dce0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3726f1851a6a59217948",
+    "url": "/prmaloy/Surreal.git/static/js/17.82e36f7d.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/prmaloy/Surreal.git/static/js/17.82e36f7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ed6dc022c0ed7c7ddbb",
+    "url": "/prmaloy/Surreal.git/static/js/18.bb9137aa.chunk.js"
+  },
+  {
+    "revision": "d61f75c3c02bb579385e",
+    "url": "/prmaloy/Surreal.git/static/js/19.88e4b270.chunk.js"
+  },
+  {
+    "revision": "a58426b6659baa7540ea",
+    "url": "/prmaloy/Surreal.git/static/js/20.129103e2.chunk.js"
+  },
+  {
+    "revision": "de7c9892fec939e0bbe8",
+    "url": "/prmaloy/Surreal.git/static/js/3.07f8b3ec.chunk.js"
+  },
+  {
+    "revision": "000d6c36287224a7ff48f88e64636454",
+    "url": "/prmaloy/Surreal.git/static/js/3.07f8b3ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "506239736e296c6198a1",
+    "url": "/prmaloy/Surreal.git/static/js/4.3ffd229f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/prmaloy/Surreal.git/static/js/4.3ffd229f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2581d54279af53bb73e3",
+    "url": "/prmaloy/Surreal.git/static/js/5.1a71bb4e.chunk.js"
+  },
+  {
+    "revision": "d2b85c3d6cd12d635845060f24eec4f9",
+    "url": "/prmaloy/Surreal.git/static/js/5.1a71bb4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07bff72cab63efdcba7d",
+    "url": "/prmaloy/Surreal.git/static/js/6.b5cf79d8.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/prmaloy/Surreal.git/static/js/6.b5cf79d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b3e2dc346a848635f9c",
+    "url": "/prmaloy/Surreal.git/static/js/7.2e53a4b3.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/prmaloy/Surreal.git/static/js/7.2e53a4b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b561ba480d3f032db3a",
+    "url": "/prmaloy/Surreal.git/static/js/8.a3c168b0.chunk.js"
+  },
+  {
+    "revision": "d052b541ae580cc0162e",
+    "url": "/prmaloy/Surreal.git/static/js/9.012402b3.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/prmaloy/Surreal.git/static/js/9.012402b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11a2d57a25257a8dad00",
+    "url": "/prmaloy/Surreal.git/static/js/main.9bc61635.chunk.js"
+  },
+  {
+    "revision": "020ec5005792fd806033",
+    "url": "/prmaloy/Surreal.git/static/js/runtime-main.2e63d6ca.js"
+  },
+  {
+    "revision": "9a449a775c160bc4f5540184a5996cb6",
+    "url": "/prmaloy/Surreal.git/static/media/bg-1.9a449a77.jpg"
+  },
+  {
+    "revision": "5abe6bb7c6f89812680774a2e1b20e36",
+    "url": "/prmaloy/Surreal.git/static/media/bg-2.5abe6bb7.jpg"
+  },
+  {
+    "revision": "9a449a775c160bc4f5540184a5996cb6",
+    "url": "/prmaloy/Surreal.git/static/media/bg-3.9a449a77.jpg"
+  },
+  {
+    "revision": "95f7463572bb5c71292f8202e5a2334a",
+    "url": "/prmaloy/Surreal.git/static/media/img-1.95f74635.jpg"
+  },
+  {
+    "revision": "bcb82f125e0b84eb3dc5e159792c3b59",
+    "url": "/prmaloy/Surreal.git/static/media/img-1.bcb82f12.jpg"
+  },
+  {
+    "revision": "34bee1aa2e21e741ba7545e2bb4a4c36",
+    "url": "/prmaloy/Surreal.git/static/media/img-10.34bee1aa.jpg"
+  },
+  {
+    "revision": "6d215168b670ca76f05ddf2151792c76",
+    "url": "/prmaloy/Surreal.git/static/media/img-2.6d215168.jpg"
+  },
+  {
+    "revision": "7fae8b3bd32912dd7200f64da283ff1e",
+    "url": "/prmaloy/Surreal.git/static/media/img-2.7fae8b3b.jpg"
+  },
+  {
+    "revision": "86fad796aff4058c5dda72a5f00651c3",
+    "url": "/prmaloy/Surreal.git/static/media/img-3.86fad796.jpg"
+  },
+  {
+    "revision": "936f586ceea98a549e980acd99fa4859",
+    "url": "/prmaloy/Surreal.git/static/media/img-3.936f586c.jpg"
+  },
+  {
+    "revision": "6c0fb5b515d8ae207554045d26dede9a",
+    "url": "/prmaloy/Surreal.git/static/media/img-4.6c0fb5b5.jpg"
+  },
+  {
+    "revision": "c512e8fb887d1a675f407bf03d204f54",
+    "url": "/prmaloy/Surreal.git/static/media/img-6.c512e8fb.jpg"
+  },
+  {
+    "revision": "3fd0f4779f1083e0d05a8791ebc3e33f",
+    "url": "/prmaloy/Surreal.git/static/media/img-8.3fd0f477.jpg"
+  },
+  {
+    "revision": "3a6f4396eb78afe106cbb62774805c22",
+    "url": "/prmaloy/Surreal.git/static/media/logodark.3a6f4396.svg"
+  },
+  {
+    "revision": "2c738040cdfc7ccfa6a005906b0cd4a6",
+    "url": "/prmaloy/Surreal.git/static/media/logolight.2c738040.svg"
+  },
+  {
+    "revision": "304ef12ea00461a2498b686d956a5ae8",
+    "url": "/prmaloy/Surreal.git/static/media/map.304ef12e.png"
+  },
+  {
+    "revision": "187554eb3597e6ac3102b388b4038662",
+    "url": "/prmaloy/Surreal.git/static/media/materialdesignicons-webfont.187554eb.eot"
+  },
+  {
+    "revision": "a5a63c099747d5e3167dd87d1bef90ce",
+    "url": "/prmaloy/Surreal.git/static/media/materialdesignicons-webfont.a5a63c09.woff"
+  },
+  {
+    "revision": "af695aec1806605828e23a5afcf06f55",
+    "url": "/prmaloy/Surreal.git/static/media/materialdesignicons-webfont.af695aec.ttf"
+  },
+  {
+    "revision": "b65a7ff4c913b0ae8b715a3629fa463f",
+    "url": "/prmaloy/Surreal.git/static/media/materialdesignicons-webfont.b65a7ff4.svg"
+  },
+  {
+    "revision": "d1bdfb3838e2f78edf1ede85f56eabc9",
+    "url": "/prmaloy/Surreal.git/static/media/materialdesignicons-webfont.d1bdfb38.woff2"
+  },
+  {
+    "revision": "1cf6217d7b5713525bc633952944f09c",
+    "url": "/prmaloy/Surreal.git/static/media/surreal5.1cf6217d.jpg"
+  },
+  {
+    "revision": "6a12e7aa5b6cc11317b83d12cd1f0aa1",
+    "url": "/prmaloy/Surreal.git/static/media/surrealfeature.6a12e7aa.jpg"
+  },
+  {
+    "revision": "a469b81ef5f238e681c93c1870e9c79a",
+    "url": "/prmaloy/Surreal.git/static/media/surrealfeature2.a469b81e.jpg"
+  },
+  {
+    "revision": "a34ecf3fb98ba6bc1537493dabafe016",
+    "url": "/prmaloy/Surreal.git/static/media/surrealfeature3.a34ecf3f.jpg"
+  },
+  {
+    "revision": "69b8451dd43b6365dd767f7684f6cff9",
+    "url": "/prmaloy/Surreal.git/static/media/surrealfeature4.69b8451d.jpg"
+  },
+  {
+    "revision": "d93e38df8ffd254a13809169beb0b46b",
+    "url": "/prmaloy/Surreal.git/static/media/surrealpackage.d93e38df.jpg"
+  },
+  {
+    "revision": "1bab0929b6839d6f633c49fcfdcec8ad",
+    "url": "/prmaloy/Surreal.git/static/media/surrealpackage1.1bab0929.jpg"
+  },
+  {
+    "revision": "3b2d446c8c0b4cecfe6028f837a52ca1",
+    "url": "/prmaloy/Surreal.git/static/media/surrealpackage2.3b2d446c.jpg"
+  },
+  {
+    "revision": "02622d7e5dd1f373361c76a7c61e1873",
+    "url": "/prmaloy/Surreal.git/static/media/surrealpackage3.02622d7e.jpg"
+  },
+  {
+    "revision": "48578ff84dd933cc00acafa7eef4168b",
+    "url": "/prmaloy/Surreal.git/static/media/surrealpackage5.48578ff8.jpg"
+  },
+  {
+    "revision": "dbd772ca8d8afac89cfa031e2b8908de",
+    "url": "/prmaloy/Surreal.git/static/media/surrealpackage6.dbd772ca.jpg"
+  }
+]);
